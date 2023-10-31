6982fdd93417632185018b7d51d2b63ff9007ba2
Ekaterina Egorova <ekaterinan.egorova@gmail.com>
2023-10-31 02:48:22 -0400

    add comments and install nodemon

--- express_server.js
+++ express_server.js
@@ -7,77 +7,68 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-
-
+// config
 app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 
+// get request to /
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// UrlDatabase in json format
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-
+// route to /hello
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+// route to urls using templates
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+// get request to new form
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//get single link from url  database
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-  
   res.render("urls_show", templateVars);
 });
+
+// using shortURL go to longURL
 app.get("/u/:id", (req, res) => {
-  const id = req.params.id
   const longURL = urlDatabase[req.params.id]
   res.redirect(longURL);
 });
 
+// add new link to urlDatabase(handle form submit)
 app.post("/urls", (req, res) => {
   const id = generateRandomString();
   urlDatabase[id] = req.body.longURL;
   res.redirect(`/urls/${id}`);
-  // console.log(urlDatabase);
-  // console.log(req.body); // Log the POST request body to the console
-  // res.send("Ok"); // Respond with 'Ok' (we will replace this)
+
 });
 
-app.set("view engine", "ejs");
-// app.get("/set", (req, res) => {
-//   const a = 1;
-//   res.send(`a = ${a}`);
-//  });
- 
-//  app.get("/fetch", (req, res) => {
-//   res.send(`a = ${a}`);
-//  });
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
 
-
+//generate random id
   function generateRandomString() {
-    const characters = 'abcdefghijklmnopqrstuvwxyzZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXY';
-    let shortUrl = [];
-    for (let i = 0; i < 6; i++) {
-      shortUrl.push(Math.floor(Math.random() * characters.length))
-  
-    }
-  
-  return shortUrl.map(el => characters.charAt(el)).join("")
-  
-  
+    const result = Math.random().toString(36).substring(8)//set of [0-9,A-Z]
+    return result
+ 
   }
 
 
