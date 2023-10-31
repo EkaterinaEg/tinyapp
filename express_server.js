e4ce16482b01f8d4016e7a516d6ea30cd3bf14d2
Ekaterina Egorova <ekaterinan.egorova@gmail.com>
2023-10-30 21:02:25 -0400

    handle info from form submit

--- express_server.js
+++ express_server.js
@@ -6,6 +6,8 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+app.use(express.urlencoded({ extended: true }));
+
 
 
 app.get("/", (req, res) => {
@@ -23,6 +25,9 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
@@ -31,7 +36,10 @@ app.get("/urls/:id", (req, res) => {
 });
 
 // req.params.id[id]
-
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.set("view engine", "ejs");
 // app.get("/set", (req, res) => {
